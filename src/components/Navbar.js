1e7cb75f4eb06a4b8f6ea9c902331baeb34c06e2
Uttam Gawas <uttam@whizti.com>
2026-05-31 18:50:57 +0530

    4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react";
 function Navbar() {
   return (
     <nav>
-      <a href="/">Posts</a>{" "}
-      <a href="/users">Users</a>{" "}
-      <a href="/notifications">Notifications</a>{" "}
+      <a href="/">Posts</a>
+      <a href="/users">Users</a>
+      <a href="/notifications">Notifications</a>
       <a href="/post">Create Post</a>
     </nav>
   );
