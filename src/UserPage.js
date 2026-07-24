1f7496da781d12deeb3a1ec0a057aed1f5bcd8c0
Uttam Gawas <uttam@whizti.com>
2026-07-24 17:13:23 +0530

    saujhas

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,40 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-function UserPage() {
-  const { userId } = useParams();
-
+function UsersPage() {
   const users = useSelector((state) => state.users);
-  const posts = useSelector((state) => state.posts);
-
-  const user = users.find((u) => u.id === userId);
-
-  const userPosts = posts.filter((post) => post.author === userId);
+  const history = useHistory();
 
   return (
     <div>
-      <h2>{user ? user.name : "User"}'s Posts</h2>
+      <h2>Users</h2>
 
-      {userPosts.length === 0 ? (
-        <p>No posts found.</p>
-      ) : (
-        <div className="posts-list">
-          {userPosts.map((post) => (
-            <div key={post.id} className="post">
-              <h3>{post.title}</h3>
-
-              <p>{post.content}</p>
-
-              <Link to={`/posts/${post.id}`}>
-                <button className="button">Edit</button>
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
+      <ul>
+        {users.map((user) => (
+          <li
+            key={user.id}
+            onClick={() => history.push(`/users/${user.id}`)}
+            style={{ cursor: "pointer" }}
+          >
+            {user.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default UserPage;
+export default UsersPage;
