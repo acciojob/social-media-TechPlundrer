6dea8155ef2111b9385e6c4dbc3039566d536f3e
Uttam Gawas <uttam@whizti.com>
2026-07-24 16:08:13 +0530

    sadas

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>GenZ</h1>
-
+<div></div>
       <nav>
         <Link to="/">Posts</Link>{" | "}
         <Link to="/users">Users</Link>{" | "}
