956dad15648c86bde00c2a3492bbca2c5333de69
Uttam Gawas <uttam@whizti.com>
2026-07-24 17:06:33 +0530

    shs

--- src/components/UsersPage.js
+++ src/components/UsersPage.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function UsersPage() {
   const users = useSelector((state) => state.users);
-  const history = useHistory();
 
   return (
     <div>
       <h2>Users</h2>
 
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className="post"
-          onClick={() => history.push(`/users/${user.id}`)}
-          style={{ cursor: "pointer", marginBottom: "15px" }}
-        >
-          <h3>{user.name}</h3>
-        </div>
-      ))}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>
+              {user.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
